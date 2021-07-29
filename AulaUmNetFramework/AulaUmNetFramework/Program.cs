6321af1c1d3d762d65a3ea87c67c426f3cc17372
unknown <josevictormobile@gmail.com>
2021-07-28 21:52:45 -0300

    Variaveis e Operacoes

---
+++ AulaUmNetFramework/AulaUmNetFramework/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaUmNetFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1;
+            double nota2;
+            double resultado;
+            Console.WriteLine("PROGRAMA DE SOMA");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("OLÁ PROFERSSORA VALKIRIA!\n\n");
+            Console.WriteLine("DIGITE A PRIMEIRA NOTA:");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("DIGITE A SEGUNDA NOTA:");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+            resultado = nota1 + nota2;
+            Console.WriteLine("O RESULTADO É: "+ resultado);
+        }
+    }
+}
