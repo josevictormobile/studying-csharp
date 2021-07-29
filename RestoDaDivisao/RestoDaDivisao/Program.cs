65f6fd67945b31c1c70f7ae4148f1c5aab7466e0
unknown <josevictormobile@gmail.com>
2021-07-28 22:03:42 -0300

    Exercitanto Operador de Resto da Divisão

---
+++ RestoDaDivisao/RestoDaDivisao/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoDaDivisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int varia1, varia2, resultado;
+            Console.WriteLine("Digite um número: ");
+            varia1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite um número: ");
+            varia2 = Convert.ToInt32(Console.ReadLine());
+            resultado = varia1 % varia2;
+
+            Console.WriteLine("O resto da divisão do primeiro pelo segundo é: " + resultado);
+        }
+    }
+}
