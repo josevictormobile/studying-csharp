bfdfe3715d08cb183a01b1b0dbbe19b623d522ad
unknown <josevictormobile@gmail.com>
2021-08-01 16:36:55 -0300

    Corrigido problema de escrita em parâmetros com uso de objetos do tipo ExpandoObject

--- ObjectDynamics/ObjectDynamics/Program.cs
+++ ObjectDynamics/ObjectDynamics/Program.cs
@@ -19,32 +19,27 @@ namespace ObjectDynamics
             qtdAlunos = Convert.ToInt32(Console.ReadLine());
             Console.Clear();
 
-            for(int i=0; i < qtdAlunos; i++)
+            for (int i = 0; i < qtdAlunos; i++)
             {
                 int indice = i + 1;
-                alunos.Add(new//POR ALGUM MOTIVO ESSE TIPO DE CRIAÇÃO DE OBJETO GERA OBJETOS COM PARÂMETROS QUE NÃO PODEM SER ESCRITOS APENAS LIDOS
-                {
-                    nome = "",
-                    matricula = 0,
-                    notas = new List<double>() { 0, 0, 0, 0 },
-                    media = 0,
-                    status = ""
-                });
+                alunos.Add(new ExpandoObject());
+                alunos[i].notas = new List<double>() { 0, 0, 0, 0 };
+                alunos[i].media = 0.0;
                 Console.WriteLine("ALUNO Nº " + indice + " FOI CRIADO COM SUCESSO!");
             }
             Console.ReadKey();
             Console.Clear();
             Console.WriteLine("----------CADASTRO DE ALUNOS----------");
-            for (int j=0; j < qtdAlunos;j++)
+            for (int j = 0; j < qtdAlunos; j++)
             {
                 Console.WriteLine("INSIRA O NOME DO ALUNO: ");
                 alunos[j].nome = Console.ReadLine();
                 Console.WriteLine("INSIRA A MATRÍCULA DO ALUNO: ");
                 alunos[j].matricula = Convert.ToInt32(Console.ReadLine());
-                for(int i=0; i< QTD_NOTAS; i++)
+                for (int i = 0; i < QTD_NOTAS; i++)
                 {
                     int indice = i + 1;
-                    Console.WriteLine("INSIRA A "+indice+"º NOTA DO ALUNO: ");
+                    Console.WriteLine("INSIRA A " + indice + "º NOTA DO ALUNO: ");
                     alunos[j].notas[i] = Convert.ToDouble(Console.ReadLine());
                     alunos[j].media += alunos[j].notas[i];
                 }
@@ -57,12 +52,12 @@ namespace ObjectDynamics
             foreach (var aluno in alunos)
             {
                 Console.WriteLine("------------------------------------");
-                Console.WriteLine("Nome: "+aluno.nome);
+                Console.WriteLine("Nome: " + aluno.nome);
                 Console.WriteLine("Matrícula: " + aluno.matricula);
                 Console.Write("Notas: ");
-                foreach(double nota in aluno.notas)
+                foreach (double nota in aluno.notas)
                 {
-                    Console.Write(nota+", ");
+                    Console.Write(nota + ", ");
                 }
                 Console.WriteLine();
                 Console.WriteLine("Média: " + aluno.media);
