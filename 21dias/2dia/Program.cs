9f27410e857b358a0e1a5f7c92913144057e54df
unknown <josevictormobile@gmail.com>
2021-08-10 06:40:22 -0300

    Revisando switch-case

--- 21dias/2dia/Program.cs
+++ 21dias/2dia/Program.cs
@@ -6,26 +6,41 @@ namespace _1dia
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Digite o primeiro número: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insira a sua escolha: ");
+            string option = Console.ReadLine();
 
-            Console.WriteLine("Digite o segundo número: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-
-            var soma = num1 + num2;
-
-            Console.WriteLine($"Digite o valor da soma dos números {num1} + {num2}: ");
-            int achoSoma = Convert.ToInt32(Console.ReadLine());
-
-            if (achoSoma == soma)
-            {
-                Console.WriteLine("Você é muito bom em matemática, está correto!");
+            switch(option){
+                case "1":
+                    Console.WriteLine("Sábia decisão");
+                    break;
+                case "2":
+                    Console.WriteLine("Not that wise my man :(");
+                    break;
+                default:
+                    Console.WriteLine("Você ainda não escolheu a opção secreta");
+                    break;
             }
-            else
-            {
-                Console.WriteLine("tsc tsc precisando praticar matemática");
-            }
-            Console.WriteLine($"A soma dos números é: {soma}");
+
+            // Console.WriteLine("Digite o primeiro número: ");
+            // int num1 = Convert.ToInt32(Console.ReadLine());
+
+            // Console.WriteLine("Digite o segundo número: ");
+            // int num2 = Convert.ToInt32(Console.ReadLine());
+
+            // var soma = num1 + num2;
+
+            // Console.WriteLine($"Digite o valor da soma dos números {num1} + {num2}: ");
+            // int achoSoma = Convert.ToInt32(Console.ReadLine());
+
+            // if (achoSoma == soma)
+            // {
+            //     Console.WriteLine("Você é muito bom em matemática, está correto!");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("tsc tsc precisando praticar matemática");
+            // }
+            // Console.WriteLine($"A soma dos números é: {soma}");
         }
     }
 }
