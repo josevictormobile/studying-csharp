0e23cbffb900c515bab6244b683685814cd121ec
unknown <josevictormobile@gmail.com>
2021-08-10 05:53:39 -0300

    Segundo dia, desafio 21 dias C# API

---
+++ 21dias/2dia/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1dia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            var soma = num1 + num2;
+
+            Console.WriteLine($"A soma dos números é: {soma}");
+        }
+    }
+}
