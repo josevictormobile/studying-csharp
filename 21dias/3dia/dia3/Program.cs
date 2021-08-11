b2cfcef50a58856a236b0ea78b344f842f2589f1
unknown <josevictormobile@gmail.com>
2021-08-11 06:29:37 -0300

    revisando loop e condicional  - desafio 21 dias

---
+++ 21dias/3dia/dia3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace dia3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Digite \n1 - sair\n2 - continuar \n3 - continuar sem a palavra 'continue':");
+                int resposta = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Sábia decisão");
+                if (resposta == 1) { break; }
+                if (resposta == 2) { continue; }
+                Console.WriteLine("Passou por aqui :)");
+                Console.WriteLine("Adicionando mais uma linha para testar o build");
+            } while (true);
+            bool outraresposta = true; // Essa variável não é a mesma da anterior pois está no escopo acima
+            if (outraresposta == true)
+            {
+                Console.WriteLine("Mais uma linha para testar o build");
+            }
+        }
+    }
+}
