58c3dffbaf17859b8e24eb0d75201c6570d42f07
Your Name <jose.figueiredo@aec.com.br>
2021-10-18 09:18:49 -0300

    Exercício URI 1070

---
+++ Exercicio/Exercicio/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            while (count < 6)
+            {
+                if (x % 2 != 0)
+                {
+                    Console.WriteLine(x);
+                    count++;
+                }
+                x++;
+            }
+        }
+    }
+}
